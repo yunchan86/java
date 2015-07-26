152ea5602be559d01927b61132ae5277ae450839
chy <yunchan86@163.com>
2015-07-26 17:30:34 +0800

    test commit

---
+++ src/main/java/core/Test.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Mavn test.");
+	}
+
+}
---
+++ src/test/java/TTest.java
@@ -0,0 +1,10 @@
+
+
+public class TTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
